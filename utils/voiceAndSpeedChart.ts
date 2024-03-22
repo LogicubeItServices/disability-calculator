180471fb3e9954806b94a9194020ea6d3361d61b
exlaso <vedant@logicubeit.com>
2024-03-22 17:22:38 +0530

    Voice and speech calc is created

--- utils/voiceAndSpeedChart.ts
+++ utils/voiceAndSpeedChart.ts
@@ -1,4 +1,4 @@
-const generalChart = (e: number) => {
+export const generalChart = (e: number) => {
   if (e == 1) return [0, 15];
   if (e == 2) return [16 - 30];
   if (e == 3) return [31 - 39];
@@ -6,4 +6,5 @@ const generalChart = (e: number) => {
   if (e == 5) return [56 - 75];
   if (e == 6) return [76 - 89];
   if (e == 7) return [90 - 100];
+  throw new Error("Invalid input");
 };
