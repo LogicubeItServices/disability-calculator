0a4b4d5ff01f5ec4fcc8dbe9ab3d0af68aaf0fc1
KrishLC575 <krish@logicubeit.com>
2024-03-23 16:16:39 +0530

    improved

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import GetCertificate from "@/components/GetCertificate";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <GetCertificate />;
+return redirect("/hearing")
 }
